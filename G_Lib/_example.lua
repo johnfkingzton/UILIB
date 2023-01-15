09f969a41b654ebe1529c17afbc30fc99cf4e7b9
johnfkingzton <70439253+johnfkingzton@users.noreply.github.com>
2023-01-14 23:51:17 -0800

    Update _example.lua

--- G_Lib/_example.lua
+++ G_Lib/_example.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/johnfkingzton/UILIB/main/G_Lib/main.lua'))()
 
 local Window = library:CreateWindow("Name", "Version", 10044538000)
 
